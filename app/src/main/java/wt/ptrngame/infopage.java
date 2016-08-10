0aa06efe8f0d4a1dc8657d65cd28a709a1ab68d6
Joel <joel.alanko@hotmail.com>
2016-08-10 18:01:15 +0300

    21 lvl restart and saving works

--- app/src/main/java/wt/ptrngame/infopage.java
+++ app/src/main/java/wt/ptrngame/infopage.java
@@ -9,8 +9,6 @@ import android.media.MediaPlayer;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
